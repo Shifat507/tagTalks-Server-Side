bbdb2c1ec04281a273d787d2cf6572eef5a47b1a
Shifat507 <ferdoushasanshifat@gmail.com>
2025-01-15 22:10:43 +0600

    added get all post API

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vyhfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,10 +23,25 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
+    const postCollection = client.db("tagTalksDb").collection("posts")
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
+
+        app.get('/post', async (req, res) => {
+            const result = await postCollection.find().sort({ createdAt: -1 }).toArray(); 
+            res.send(result);
+        });
+
+        app.post('/post', async(req, res)=>{
+            const postData = req.body;
+            postData.createdAt = new Date();
+            const result = await postCollection.insertOne(postData);
+            res.send(result);
+        })
+
 
 
         await client.db("admin").command({ ping: 1 });
